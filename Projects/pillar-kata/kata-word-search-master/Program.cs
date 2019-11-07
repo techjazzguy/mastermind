70c6267cc68e051c3e9d2c9488c1c807ff2c5093
Eric Alberts <techjazzguy@gmail.com>
2019-11-07 18:41:19 -0500

    Read preformatted text lines of input file.

--- Projects/pillar-kata/kata-word-search-master/Program.cs
+++ Projects/pillar-kata/kata-word-search-master/Program.cs
@@ -1,15 +1,46 @@
 ﻿using System;
 using System.IO;
+using System.Xml;
+
     class Program
     {
-        public static string fileName="wordsearch.xml";
+          public static XmlDocument wordSearchDoc = new XmlDocument();
+          public static XmlNode xmlNode;
+          public static string wordSearchDocPath = "/Users/eric/Documents/git/Projects/pillar-kata/kata-word-search-master/bin/Debug/netcoreapp3.0/wordsearch.xml";
         static void Main(string[] args)
         {
-            Program.OpenFile();
-        }
+            
+            //Check if file exists.
+            if (!File.Exists(Program.wordSearchDocPath))
+            {
+                Console.WriteLine("Input file must exist.");
+                return;
+            }
 
-        public static void OpenFile()
-        {
-            File.ReadAllText(Program.fileName);
-        }
+            try
+            {
+                Program.wordSearchDoc.Load(Program.wordSearchDocPath);
+            }
+            catch
+            {
+                Console.WriteLine("An error occurred while loading the word search file.");
+                return;
+            }
+
+            if (Program.wordSearchDoc.InnerText.Length == 0)
+            {
+                Console.WriteLine("Input file is empty.");
+                return;
+            }
+            Program.xmlNode = wordSearchDoc.SelectSingleNode("pre").FirstChild;
+
+                using (StringReader reader = new StringReader(Program.xmlNode.InnerText))
+                {
+                    string line;
+                    while (( line = reader.ReadLine()) != null)
+                    {
+                        Console.WriteLine("Test " + line);
+                    }
+                }
+}
     }
--- Projects/pillar-kata/kata-word-search-master/UnitTests.cs
+++ Projects/pillar-kata/kata-word-search-master/UnitTests.cs
@@ -1,12 +1,32 @@
 using Xunit;
 using System.IO;
+using System.Xml;
 
     public class UnitTests
     {
         [Fact]
         public void InputFileExists()
         { 
-           Assert.True(File.Exists(Program.fileName));
+           Assert.True(File.Exists(Program.wordSearchDocPath));
         }
+        [Fact]
+        public void InputFileIsNotEmpty()
+        { 
+           FileInfo fi = new FileInfo(Program.wordSearchDocPath);
+           Assert.True(fi.Length > 0);
+        }
+         [Fact]
+        public void CanLoadInputFile()
+        {
+          XmlDocument wordSearchDoc = new XmlDocument();
+          Program.wordSearchDoc.Load(Program.wordSearchDocPath);
+        }
+        [Fact]
+        public void InputFileContainsPreformattedText()
+        { 
+           Program.xmlNode = Program.wordSearchDoc.SelectSingleNode("pre").FirstChild;
+           Assert.True(Program.xmlNode.InnerText != null);
+        }
+        
     }
  
