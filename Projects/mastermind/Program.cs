8c645be95287a48ef81015e2aabfe9c488d40cf9
Eric Alberts <techjazzguy@gmail.com>
2019-11-23 19:31:25 -0500

    Checked in a basic program. Checks guesses.

--- Projects/mastermind/Program.cs
+++ Projects/mastermind/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using MastermindNamespace;
-    class Program
+class Program
+{
+
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        // Start the game. Prompt the user to start.
+        Console.WriteLine("Welcome to Mastermind. Press the enter key to start playing.");
+        Console.Read();
+        //Initialize new instance of the Mastermind game and generate a code to guess.
+        Mastermind mastermind = new Mastermind();
+        //Console.WriteLine(mastermind.GenerateCode());
+        Console.WriteLine("A " + mastermind.maxCodeNumbers + 
+        " digit code was generated with each digit between " 
+        + mastermind.minNumber + " and " + mastermind.maxNumber + ". Guess the code!");
+        int numGuesses = 0;
+        for (int i = numGuesses; i <= mastermind.maxGuesses; i++)
         {
-            Console.WriteLine("Welcome to Mastermind. Press the enter key to start playing.");
-            Console.Read();
-            Mastermind mastermind = new Mastermind();
-            Console.WriteLine(mastermind.GenerateCode());
+            string guess = Console.ReadLine();
+            Console.WriteLine(mastermind.GuessAnswer(guess));
+            if (mastermind.isCorrectAnswer)
+            {
+                break;
+            }
+        }
+        // If the correct answer was not guessed after the maximum number of tries, then end the game.
+        if (!mastermind.isCorrectAnswer)
+        {
+            Console.WriteLine("Sorry, the code was not correctly guessed! Game over.");
         }
     }
+}
--- Projects/mastermind/mastermind.cs
+++ Projects/mastermind/mastermind.cs
@@ -4,14 +4,19 @@ namespace MastermindNamespace
 {
     public class Mastermind
     {
-        string Code = String.Empty;
-        const int maxCodeNumbers = 4;
-        
+        public string Code = String.Empty;
+        public int maxCodeNumbers = 4;
+        public int maxGuesses = 10;
+        public int minNumber = 1;
+        public int maxNumber = 6;
+        public bool isCorrectAnswer = false;
+
         public string GenerateCode()
         {
             Code = String.Empty;
             const int minNumber = 1;
             const int maxNumber = 6;
+            // Generate a string a random numbers by looping thru each number until max number of digits is reached.
             for (int i = 1; i <= maxCodeNumbers; i++)
             {
                 Random randomNum = new Random();
@@ -20,5 +25,21 @@ namespace MastermindNamespace
             return Code;
         }
 
+        public string GuessAnswer(string guess)
+        {
+            if (guess == Code)
+            {
+                // Set correct answer flag to true.
+                this.isCorrectAnswer = true;
+                return "You guessed the code correctly! Good job.";
+            }
+            if (String.IsNullOrEmpty(guess))
+            {
+                return "Invalid guess";
+            }
+            // Ensure correct answer flag is true.
+            this.isCorrectAnswer = false;
+            return "Invalid guess";
+        }
     }
 }
